e031f517390378a2bc89cdc4ac301134c0607a50
Acer <codecinnpro@gmail.com>
2024-04-29 23:26:01 +0800

    1. first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/input',function(req,res,next){
+  res.render('input', {title: 'Input data to MongoDB'})
+});
+
+// router.post('/input', function(req,res,next){
+
+// })
+
+module.exports = router;
