27300571e753b0c06e7fd76b51b349a7d7ec9930
Acer <codecinnpro@gmail.com>
2024-05-02 02:41:53 +0800

    5. datalog dashboard paga switching

--- controllers/iot.js
+++ controllers/iot.js
@@ -24,3 +24,32 @@ module.exports.switchCollection = async (req,res, next)=>{
     //can called data.locationName 
     res.render('iot/dashboard',  { data});
 }
+
+module.exports.dataLogDisplay = async(req,res,next)=>{
+    const {id} = req.params;
+    console.log("collection name: ",id);
+    let locationName;
+    switch(id){
+        case 'ayerkeroh':
+            locationName = "Ayer Keroh";
+            break;
+        case 'duriantunggal':
+            locationName = "Durian Tunggal";
+            break;
+        case 'alorgajah':
+            locationName = "Alor Gajah";
+            break;
+        default:
+            return res.status(400).json({ error: 'Invalid collection id' });
+    }
+    const data = {id, locationName}
+    //can called data.locationName 
+    res.render('iot/datalog',  { data}); 
+}
+
+module.exports.notificationDisplay =(req,res)=>{
+
+    //store value using session
+    // https://youtu.be/GihQAC1I39Q?si=eRTkXeeawOS7I1ij
+    res.render('iot/notification');
+}
--- routes/iot.js
+++ routes/iot.js
@@ -5,6 +5,10 @@ var iot = require('../controllers/iot');
 router.route('/dashboard/:id')
     .get(iot.switchCollection);
 
+router.route('/datalog/:id')
+    .get(iot.dataLogDisplay);
 
+router.route('/notification')
+    .get(iot.notificationDisplay);
 
 module.exports = router;
