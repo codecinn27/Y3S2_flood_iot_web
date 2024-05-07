139333a41cfa61ca520bbd806a09349b8190c30e
Acer <codecinnpro@gmail.com>
2024-05-08 02:35:50 +0800

    9. real time 1 sensor detection at /crud/renderingDataFavoriot

--- controllers/crud.js
+++ controllers/crud.js
@@ -75,22 +75,23 @@ module.exports.readDataFavoriot = async(req,res)=>{
 
 module.exports.getDataFavoriot = async(req,res)=>{
     try {
-        const response = await axios.get('https://apiv2.favoriot.com/v2/streams?device_developer_id=dht11_test_1_device@iiotsme&max=1&order=DESC', {
+        const response = await axios.get('https://apiv2.favoriot.com/v2/streams?device_developer_id=flood_dev_env_1_device@iiotsme&max=1&order=DESC', {
           headers: {
             'cache-control': 'no-cache',
             'content-type': 'application/json',
-            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imlpb3RzbWUiLCJyZWFkX3dyaXRlIjp0cnVlLCJpYXQiOjE3MTMyMzA0NTh9.NAgZ2-4KxSZjwGKr-CWKPc8ZMEMDikqVh5rHO_wOMOM'
+            'apikey': '5PVlogIoauXc1ihGC0u6yRYlmlALX1aE'
           }
         });
-    
-        const results = response.data.results;
-    
-        const formattedResults = results.map(result => ({
-          timestamp: formatTimestamp(result.timestamp),
-          data: result.data
+        //console.log("response: ",response);
+        const result = response.data.results;
+        //console.log("result",result);
+        const formattedData = result.map(item => ({
+            data: item.data,
+            timestamp: formatTimestamp(item.timestamp),
         }));
-    
-        res.json(formattedResults);
+        
+        //console.log(formattedData);
+        res.json(formattedData);
       } catch (error) {
         console.error('Error fetching Favoriot data:', error);
         res.status(500).json({ error: 'Failed to fetch Favoriot data' });
@@ -104,8 +105,12 @@ module.exports.renderingDataFavoriot = async(req,res)=>{
         if(response.status !== 200){
             throw new Error('Failed to fetch Favoriot data');
         }else{
-            console.log(response);
-            res.render('crud/favoriot2', {response})
+            console.log("Rendering Data from Favoriot IOT",response.data);
+            const favoriotData = response.data; // Assuming response.data is an array of objects
+            // Pass both data and timestamp to the template
+            const firstItem = favoriotData[0]; // Get the first item in the array
+            // Pass the first item to the template
+            res.render('crud/favoriot2', { favoriotData:firstItem });
         }
     }catch(error){
         console.error('Error fetching data from Favoriot', error);
